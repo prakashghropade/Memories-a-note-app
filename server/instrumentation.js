44497d14cd8c2368c14286fdfac8a1b4cbf5e833
prakashghropade <prakashghorpade2001@gmail.com>
2026-08-20 15:11:09 +0530

    updated the image tag

--- server/instrumentation.js
+++ server/instrumentation.js
@@ -1,6 +1,8 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
+
 
 const traceExporter = new OTLPTraceExporter({
   url:
@@ -9,6 +11,10 @@ const traceExporter = new OTLPTraceExporter({
 });
 
 const sdk = new NodeSDK({
+   resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: "memories-api",
+  }),
+
   traceExporter,
 
   instrumentations: [
